bfdd375c82309e1ca7e7af603f81ac992bf8fca3
ryan sims <ryan.sims@gmail.com>
2014-10-27 07:00:09 -0700

    Animating launch screen to card segue

---
+++ ObliqueStrategies/LaunchViewController.swift
@@ -0,0 +1,96 @@
+//
+//  LaunchViewController.swift
+//  ObliqueStrategies
+//
+//  Created by Ryan Sims on 10/26/14.
+//  Copyright (c) 2014 Ryan Sims. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+    
+    var isPresenting: Bool = true
+
+    @IBOutlet weak var launchView: UIView!
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var cardsView: UIView!
+    @IBOutlet weak var targetUIButton: UIButton!
+    @IBOutlet weak var targetImageView: UIImageView!
+    @IBOutlet weak var smallCardsImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        UIView.animateWithDuration(1.2, delay: 0.0, options: .Autoreverse | .Repeat, animations: { () -> Void in
+            self.targetImageView.alpha = 0.3
+            }, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func targetTap(sender: AnyObject) {
+        UIView.animateWithDuration(0.75, delay: 0, options: nil, animations: { () -> Void in
+            // lift title up
+            self.titleImageView.transform = CGAffineTransformMakeTranslation(0.0, -230.0)
+            // scale mini cards and target
+            self.cardsView.transform = CGAffineTransformMakeTranslation(0.0, -130.0)
+            self.cardsView.transform = CGAffineTransformScale(self.cardsView.transform, 6.06, 6.06)
+        }) { (finished: Bool) -> Void in
+            // load our next view
+            self.performSegueWithIdentifier("cardSegue", sender: self)
+        }
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        var destinationVC = segue.destinationViewController as UIViewController
+        destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
+        destinationVC.transitioningDelegate = self
+        
+    }
+    
+    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = false
+        return self
+    }
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 0.2
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        //println("animating transition")
+        var containerView = transitionContext.containerView()
+        var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        if (isPresenting) {
+            containerView.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                toViewController.view.alpha = 1
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+            }
+        } else {
+            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                fromViewController.view.alpha = 0
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    fromViewController.view.removeFromSuperview()
+            }
+        }
+    }
+
+}
